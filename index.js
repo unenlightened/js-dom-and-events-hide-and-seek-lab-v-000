b73742d97ba6058bfb67a1ce5eab4dd2d5b133a9
Chely Ho <adept_inept@yahoo.com>
2018-07-06 19:16:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.getElementById('nested').getElementsByClassName('target')[0]
 }
+
+function increaseRankBy(n) {
+  ranks = document.querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML += n
+  }
+}
+
+const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+ 
+for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = (i + 1).toString()
+}
