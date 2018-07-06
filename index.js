e51d74c1a232e891c1282b44f1f82e7f578a0cae
Chely Ho <adept_inept@yahoo.com>
2018-07-06 19:16:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,4 @@ function increaseRankBy(n) {
   }
 }
 
-const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
- 
-for (let i = 0; i < lis.length; i++) {
-  lis[i].innerHTML = (i + 1).toString()
-}
+
