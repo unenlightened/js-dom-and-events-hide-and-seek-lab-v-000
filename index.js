e7716d98339a1bf7f06ff2ca03d3bb251d022e5f
Chely Ho <adept_inept@yahoo.com>
2018-07-06 19:47:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ function nestedTarget() {
 function increaseRankBy(n) {
   let ranks = document.querySelectorAll('.ranked-list li')
 
-  for (let i = 0; i < ranks.length; i++) {
-    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  ranks.forEach (rank => 
+    rank.innerHTML = parseInt(rank.innerHTML) + n )
   }
 }
 
@@ -19,11 +19,33 @@ function increaseRankBy(n) {
 //   return document.querySelector('#grand-node div div div div')
 // }
 
-function deepestChild() {
-  let children = document.querySelectorAll('#grand-node')
-
-  for(let i = 0; i < children.length; i++) {
-    
-  }
-}
-
+// function deepestChild() {
+//   let children = document.querySelectorAll('#grand-node')
+//   let deepestLevel = 0
+// 
+//   return children.forEach(function (child))
+//   
+// }
+// function getAllChildren(group, children) {
+//   children = children || [];
+//   if(group && Array.isArray(group.children)) {
+//     group.children.forEach(function(child) { 
+//       getAllChildren(child, children)
+//     });
+//   }
+//   else {
+//     children.push(group);
+//   }
+//   return children;
+// }
+// 
+// var data = { children: [{ children: [{ children: [{ name: 'item1' }, { name: 'item2' }, { name: 'item3' }] }, { children: [{ name: 'item1' }, { name: 'item2' }, { name: 'item3' }] }, { children: [{ name: 'item1' }, { name: 'item2' }, { name: 'item3' }] }] }, { children: [{ children: [{ name: 'item1' }] }, { children: [{ name: 'item1' }] }, { children: [{ name: 'item1' }] }] }, { children: [{ children: [{ name: 'item1' }, { name: 'item2' }, { name: 'item3' }] }, { children: [{ name: 'item1' }, { name: 'item2' }, { name: 'item3' }] }, { children: [{ name: 'item1' }, { name: 'item2' }, { name: 'item3' }] }] }] },
+//     children = [data].reduce(function iter(r, a) {
+//         if (Array.isArray(a.children)) {
+//             return a.children.reduce(iter, r);
+//         }
+//         r.push(a);
+//         return r;
+//     }, []);
+// 
+// console.log(children);
