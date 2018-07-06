f81c0f075e05ece146534956e6752deab1270344
Chely Ho <adept_inept@yahoo.com>
2018-07-06 19:16:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  ranks = document.querySelectorAll('ul.ranked-list li')
+  let ranks = document.querySelectorAll('ul.ranked-list li')
 
   for (let i = 0; i < ranks.length; i++) {
     ranks[i].innerHTML += n
