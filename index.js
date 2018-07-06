1442e82d1378fe943d54d8078f9c386dbada42a4
Chely Ho <adept_inept@yahoo.com>
2018-07-06 19:23:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  return document.querySelector('grand-node div div div div')
+  return document.querySelector('#grand-node div div div div')
 }
